63c480b751e0b271fda5cd44bb8a9213b1a3f173
Kubisz Mariusz <kontakt@kubiszmariusz.pl>
2023-08-02 21:27:50 +0200

    👀 Favicon
    The favicon and app icons are created using the tutorial from https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      title: "Kubisz Mariusz",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      htmlAttrs: {
+        lang: "pl-PL",
+      },
+      link: [
+        {
+          rel: "icon",
+          href: "/favicon.ico",
+          sizes: "32x32",
+        },
+        {
+          rel: "icon",
+          href: "/icon.svg",
+          type: "image/svg+xml",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "manifest",
+          href: "/manifest.webmanifest",
+        },
+      ],
+    },
+  },
+
+  devtools: { enabled: true },
+});
